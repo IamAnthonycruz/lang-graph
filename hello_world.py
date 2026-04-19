dcd8df242072b2a67761ccf147e80a2a59f131f0
IamAnthonyCruz <Charliecruz1224@gmail.com>
2026-04-18 22:56:59 -0400

    Create graph

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+from typing import TypedDict
+
+from langgraph.graph import StateGraph
+
+class AgentState(TypedDict):
+	message: str
+
+
+def greeting_node(state: AgentState) -> AgentState:
+	"""Simple node that adds a greeting message to the state """
+
+	state['message'] = f"Hey {state['message']}  how is your day going?"
+	return state
+
+graph = StateGraph(AgentState)
+
+graph.add_node("greeter", greeting_node)
+
+graph.set_entry_point("greeter")
+graph.set_finish_point("greeter")
+
+app = graph.compile()
